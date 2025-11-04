346302f7c160e69fe07f1a64f33c00cfb0c5a43e
Dogan Alici <doganalici2144@gmail.com>
2025-11-04 21:13:12 +0300

    Dictionary Update

---
+++ DictionaryPersonelManagementSystem/Manager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPersonelManagementSystem
+{
+    public class Manager
+    {
+        private Dictionary<int, Personnel> staff = new Dictionary<int, Personnel>();
+        public void AddPersonnel(Personnel p)
+        {
+            if (!staff.ContainsKey(p.Id))
+            {
+                staff.Add(p.Id, p);
+                Console.WriteLine($"{p.FirstName} {p.LastName} başarıyla eklenmiştir");
+            }
+            else
+            {
+                Console.WriteLine("Bu ID'ye sahip bir personel zaten mevcut.\n");
+            }
+        }
+
+        public void DeletePersonnel(int id)
+        {
+            if (staff.ContainsKey(id))
+            {
+                staff.Remove(id);
+                Console.WriteLine($"ID : {id} olan personel kayıtlardan silinmiştir");
+            }
+            else
+            {
+                Console.WriteLine("Bu ID'ye sahip personel bulunamadı.");
+            }
+        }
+        public void UpdatePersonnel(int id, Personnel newInformation) { }
+        public void ListAll()
+        {
+            if (staff.Count == 0)
+            {
+                Console.WriteLine("\nListelenecek herhangi bir personel kaydı bulunamadı!");
+                return;
+            }
+            foreach (var p in staff)
+            {
+                Console.WriteLine(p.Value);
+                Console.WriteLine(new string('*', 25));
+            }
+        }
+        public void FindPersonnel(int id) { }
+
+    }
+}
---
+++ DictionaryPersonelManagementSystem/Personnel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPersonelManagementSystem
+{
+    public class Personnel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Personnel(int id, string firstName, string lastName, string department, decimal salary, string phoneNumber)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+            Salary = salary;
+            PhoneNumber = phoneNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {Id}\nAd Soyad : {FirstName} {LastName}\nDepartman : {Department}\nMaaş : {Salary}\nTelefon Numarası : {PhoneNumber}";
+        }
+    }
+}
